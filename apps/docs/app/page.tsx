9c14053bfc439b182f8ef36ea2d0b597e92131d3
patrick.schott <patrick.schott@kittelberger.de>
2025-03-18 11:00:40 +0100

    [main] change nr

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>This is the docs Project</li>
+          <li>This is the docs Project1</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
